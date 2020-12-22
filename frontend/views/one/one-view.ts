8240facf6c5283a856a8f89b6dae617646db0b24
Marcus Hellberg <marcus@vaadin.com>
2020-12-22 14:30:15 -0800

    Add some content

--- frontend/views/one/one-view.ts
+++ frontend/views/one/one-view.ts
@@ -1,16 +1,26 @@
-import { css, customElement, html, LitElement } from 'lit-element';
+import { css, customElement, html, LitElement } from "lit-element";
+import "@vaadin/vaadin-text-field";
 
-@customElement('one-view')
+@customElement("one-view")
 export class OneView extends LitElement {
   static get styles() {
     return css`
       :host {
         display: block;
+        padding: var(--lumo-space-m);
       }
     `;
   }
 
   render() {
-    return html`<div>Content placeholder</div>`;
+    return html`
+      <h1>View One</h1>
+
+      <p>Here is some text as an example</p>
+
+      <vaadin-text-field label="Enter some text"></vaadin-text-field>
+
+      <div class="warning">This is a warning.</div>
+    `;
   }
 }
--- frontend/views/two/two-view.ts
+++ frontend/views/two/two-view.ts
@@ -1,16 +1,21 @@
-import { css, customElement, html, LitElement } from 'lit-element';
+import { css, customElement, html, LitElement } from "lit-element";
 
-@customElement('two-view')
+@customElement("two-view")
 export class TwoView extends LitElement {
   static get styles() {
     return css`
       :host {
         display: block;
+        padding: var(--lumo-space-m);
       }
     `;
   }
 
   render() {
-    return html`<div>Content placeholder</div>`;
+    return html`
+      <h1>View Two</h1>
+
+      <div class="warning">This is a warning.</div>
+    `;
   }
 }
