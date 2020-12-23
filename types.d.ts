7599f28681c6541b8d2294a450b27184701937f2
Marcus Hellberg <marcus@vaadin.com>
2020-12-22 18:03:47 -0800

    remove old type def

--- types.d.ts
+++ types.d.ts
@@ -2,9 +2,3 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
-
-declare module "*.css" {
-  import { CSSResult } from "lit-element";
-  const content: CSSResult;
-  export default content;
-}
